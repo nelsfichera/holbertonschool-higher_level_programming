46cb738adbd7c5b7b77c01f23616c36b58fedfc5
Nels <nelsfichera@gmail.com>
2021-04-28 10:01:57 -0400

    Fix indentations

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def magic_calcuation(a, b, c):
-    if (a < b):
+    if a < b:
         return c
-    elif (c > b):
+    elif c > b:
         return a + b
-    else:
-        return a * b - c
+    return a * b - c
