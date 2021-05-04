150292ee475b9ac840724da873759841076f52a5
Nels <nelsfichera@gmail.com>
2021-05-04 15:32:04 -0400

    Remove append

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    new_list = []
-    for element in my_list:
-        if element % 2 == 0:
-            new_list.append(True)
+    new = my_list[:]
+    for x in range(len(new)):
+        if new[x] % 2 == 0:
+            new[x] = True
         else:
-            new_list.append(False)
-        return new_list
+            new[x] = False
+        return new
