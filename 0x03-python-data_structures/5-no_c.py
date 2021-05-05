0af45fdfe0e5ed514a5deeafc1381f49b86efa1c
Nels <nelsfichera@gmail.com>
2021-05-05 08:21:14 -0400

    Fix variable

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    return ("".join(char for c in my_string if char not in "Cc"))
+    c_less_str = "".join([char for char in my_string if char not in "Cc"])
+    return c_less_str
