396ec2f5533c0881fe690c98959f229e6961c061
Nels <nelsfichera@gmail.com>
2021-05-05 08:19:17 -0400

    join method

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new = ""
-    for char in my_string:
-        if char == 'c' or char == 'C':
-	    continue
-            new += char
-        return new
+    return ("".join(char for c in my_string if char not in "Cc"))
