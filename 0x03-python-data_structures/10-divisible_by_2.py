f8769cfadad78b08bd882b2c5dac81e62657858b
Nels <nelsfichera@gmail.com>
2021-05-05 08:25:15 -0400

    Clean up join

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    new = my_list[:]
-    for x in range(len(new)):
-        if new[x] % 2 == 0:
-            new[x] = True
-        else:
-            new[x] = False
-        return new
+    if my_list:
+        new_list = []
+        index = 0
+        for x in my_list:
+            if x % 2 == 0:
+                new_list.append(True)
+            else:
+                new_list.append(False)
+                index += 1
+        return new_list
