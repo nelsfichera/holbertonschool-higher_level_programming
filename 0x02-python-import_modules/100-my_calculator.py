dae455fe00d3118eb97789e2653f3df64076325e
Nels <nelsfichera@gmail.com>
2021-05-07 14:14:43 -0400

    adv tasks

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    from calculator_1 import add, sub, mul, div
+
+    arggggc = len(argv)
+
+    if arggggc == 4:
+        if argv[2] == '+':
+            print("{:s} + {:s} = {:d}".format(argv[1], argv[3],
+                                              add(int(argv[1]), int(argv[3]))))
+        elif argv[2] == '-':
+            print("{:s} - {:s} = {:d}".format(argv[1], argv[3],
+                                              sub(int(argv[1]), int(argv[3]))))
+        elif argv[2] == '*':
+            print("{:s} * {:s} = {:d}".format(argv[1], argv[3],
+                                              mul(int(argv[1]), int(argv[3]))))
+        elif argv[2] == '/':
+            print("{:s} / {:s} = {:d}".format(argv[1], argv[3],
+                                              div(int(argv[1]), int(argv[3]))))
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
+    else:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
