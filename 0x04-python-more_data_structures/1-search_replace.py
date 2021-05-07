549bb7fc14eb4ad7d046fd8f5fe9c1b211eb4868
Nels <nelsfichera@gmail.com>
2021-05-07 09:53:24 -0400

    Diff approach, no append

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = []
-    for element in my_list:
-	new_list.append(element if element != search else replace)
+    new_list = my_list.copy()
+    x = 0
+    while x < len(my_list):
+        if my_list[x] == search:
+            new_list[x] = replace
+        x += 1
     return new_list
