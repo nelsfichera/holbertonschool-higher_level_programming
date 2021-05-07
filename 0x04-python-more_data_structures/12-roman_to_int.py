6080d0e4540cc264a45cabf065531decbce67b24
Nels <nelsfichera@gmail.com>
2021-05-07 10:09:46 -0400

    Justin's incredibly smart solution

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,16 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    quot_numero = {'M': 1000, 'D': 500, 'C': 100,
-                   'L': 50, 'X': 10, 'V': 5, 'I': 1}
-    mensio = 0
-    iteratio = 0
-    if isinstance(roman_string, str) and roman_string is not None:
-        while iteratio < len(roman_string):
-            if (iteratio < (len(roman_string) - 1) and
-                quot_numero[roman_string[iteratio + 1]] >
-                quot_numero[roman_string[iteratio]])
-            mensio -= quot_numero[romanstring[iteratio]]
-        else:
-            mensio += quot_numero[roman_string[iteratio]]
-            iteratio += 1
-    return mensio
+    if type(roman_string) is not str:
+        return 0
+    key = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    ints = [key[N] for N in roman_string]
+    return sum(map((lambda x, y: -x if x < y else x), ints, ints[1:] + [0]))
