3bd210edc9a46f4f2d6b6fac01512a3aaf71894a
Nels <nelsfichera@gmail.com>
2021-05-19 09:40:16 -0400

    Space

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -4,7 +4,7 @@
 
 class Square:
     """Define the square"""
-    def __init__(self, size=0, position=(0,0)):
+    def __init__(self, size=0, position=(0, 0)):
         """sets size & position"""
         self.size = size
         self.position = position
