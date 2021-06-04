e847502df689612a73651455ec3164d9fbf4a413
Nels <nelsfichera@gmail.com>
2021-06-04 10:54:54 -0400

    fix typo

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     '''reads a file passed as argument'''
-    with open(filename, endcoding='utf-8') as the_file:
+    with open(filename, encoding='utf-8') as the_file:
         print(the_file.read(), end="")
