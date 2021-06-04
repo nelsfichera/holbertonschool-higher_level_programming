c2aef6d18b5dc5b5d1f77aba53256820a5c9e88c
Nels <nelsfichera@gmail.com>
2021-06-04 10:58:41 -0400

    documentation

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,10 +3,10 @@
 import json
 import os.path
 from sys import argv
-
+'''load previous files'''
 save_to = __import__('5-save_to_json_file').save_to_json_file
 load_from = __import__('6-load_from_json_file').load_from_json_file
-
+'''add the arguments to a python list and save to file'''
 if __name__ == "__main__":
     the_list = []
     if os.path.exists("add_item.json"):
