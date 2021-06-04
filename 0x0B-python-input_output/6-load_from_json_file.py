fcc7dffe03790017ad79b2d9888bd34f8e3a52c3
Nels <nelsfichera@gmail.com>
2021-06-04 10:56:08 -0400

    longer message

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -4,6 +4,6 @@ import json
 
 
 def load_from_jston_file(filename):
-    '''does the thing'''
+    '''does the thing of creating the thing but longer json object'''
     with open(filename, encoding="utf-8") as the_file:
         return json.load(the_file)
