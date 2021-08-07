14fcfbe6c5d6bdbcdadc22266f5c53390c87cd02
Nels <nelsfichera@gmail.com>
2021-08-07 13:06:39 -0400

    fix typo

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost",
                            port=3306,
                            user=argv[1],
-                           passwd=arg[2],
+                           passwd=argv[2],
                            db=argv[3],
                            charset="utf=8")
     cur = conn.cursor()
