4f455d64ae622824b262578c1da64aebe4f2e535
Nels <nelsfichera@gmail.com>
2021-08-08 19:23:08 -0400

    left join

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     c = db.cursor()
     c.execute(
         """SELECT cities.id, cities.name, states.name FROM cities
-        JOIN states ON cities.state_id = states.id
+        LEFT JOIN states ON cities.state_id = states.id
         ORDER BY 'cities.id' ASC;"""
     )
     record = c.fetchall()
