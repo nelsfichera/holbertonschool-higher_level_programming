f936309e5953f14f02c2a77dd259952e98ba2fe1
Nels <nelsfichera@gmail.com>
2021-08-08 19:38:54 -0400

    fix

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
-'''db connection'''
-
-import sqlalchemy
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
-from sys import argv
+'''city objects from database'''
 from model_state import Base, State
+from model_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
 
-if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
-                           argv[2], argv[3]))
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1],
+        sys.argv[2],
+        sys.argv[3]), pool_pre_ping=True
+    )
     Base.metadata.create_all(engine)
-    session = sessionmaker(bind=engine)
-    rows = session.query(City, State).filter(City.state_id == State.id)\
-                                     .order_by(City.id).all()
-    for city, state in rows:
-        print("{}: ({}) {}".format(state.name, city.id, city.name))
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for s, c in session.query(State, City).filter(
+            State.id == City.state_id).all():
+        print("{}: ({}) {}".format(s.name, c.id, c.name))
     session.close()
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
 '''city class'''
-import sqlalchemy
 from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
 from model_state import Base, State
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
 
 
 class City(Base):
-    '''representation of city'''
-    __tablename__ = "cities"
-    id = Column(Integer, primary_key=True)
+    """City class that inherits from base"""
+    __tablename__ = 'cities'
+
+    id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'))
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
